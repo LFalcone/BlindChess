d6dd3eebe8744176e6ef3739cec6495a4c0b7356
Jake Terpening <jterpeni@uci.edu>
2018-05-21 22:23:10 -0700

    Revert "Revert "Board generates now""
    This reverts commit 0537c6274669ade3745735dbcac632a9ac5f35ab.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
-	public List<GameObject> tiles;
+	public List<List<GameObject>> tiles;
+	public GameObject Tile;
+
+	void Start()
+	{
+		MakeBoard (4,6);
+	}
+	void MakeBoard(int x, int y)
+	{
+		tiles = new List<List<GameObject>>();
+		for (int i = 0; i < x; ++i) 
+		{
+			for (int j = 0; j < y; ++j) 
+			{
+				GameObject myTile = Instantiate(Tile);
+				myTile.transform.parent = this.transform;
+				myTile.transform.position = new Vector3 (i, 0, j);
+				if ((i + j) % 2 == 0) 
+				{
+					Tile tileScript = myTile.GetComponent<Tile> ();
+					tileScript.setBlack ();
+				}
+			}
+		}
+	}
 }
