0537c6274669ade3745735dbcac632a9ac5f35ab
Jake Terpening <jterpeni@uci.edu>
2018-05-21 22:22:15 -0700

    Revert "Board generates now"
    This reverts commit 001defda274ab4481a9848d4f92e8bb474681b52.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,19 +7,4 @@ public class Tile : MonoBehaviour {
 	public int state = 0; 	//0=no piece, 1=white piece, 2=black piece
 	public List<int> pos;	//(x,y) bottom left tile = (0,0)
 	public GameObject piece;	//will be empty if state=0
-	public Material mat;
-
-	void Awake()
-	{
-		mat = GetComponent<Renderer>().material;
-	}
-
-	public void setBlack()
-	{
-		mat.color = Color.black;
-	}
-	public void setWhite()
-	{
-		mat.color = Color.white;
-	}
 }
