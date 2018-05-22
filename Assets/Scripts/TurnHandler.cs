af2afb4fc8b8ad25e028a57ec79766a0432ba730
Luke Falcone <lfalcone@uci.edu>
2018-05-21 19:32:26 -0700

    Created the Skeleton for a TurnHandler script

---
+++ Assets/Scripts/TurnHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour {
+	
+	public enum Player { WHITE, BLACK }
+
+	public bool startingPlayer = Player.WHITE;
+
+	private bool turnPlayer;
+	private bool waitForNextPlayer;
+	private bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+		turnPlayer = startingPlayer;
+		waitForNextPlayer = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void NextTurn () {
+		waitForNextPlayer = true;
+		turnPlayer = !turnPlayer;
+	}
+
+	void RevealTurnPlayer () {
+		waitForNextPlayer = false;
+	}
+
+	void RevealBoard () {
+		gameOver = true;
+	}
+}
