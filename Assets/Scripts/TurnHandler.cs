6ed7ad7f2882ff65bc30c7dadafcb2cf8a35cc49
Luke Falcone <lfalcone@uci.edu>
2018-05-21 21:02:41 -0700

    Added functionality for turn control. Currently we are using the enter and spacebar but these shouldbe changed to ui buttons

--- Assets/Scripts/TurnHandler.cs
+++ Assets/Scripts/TurnHandler.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turn : MonoBehaviour {
+public class TurnHandler : MonoBehaviour {
 	
 	public enum Player { WHITE, BLACK }
 	public bool whiteTurn = true;
 
-	public bool startingPlayer = true;
+	public Player startingPlayer = Player.WHITE;
 
-	private bool turnPlayer;
+	private Player turnPlayer;
 	private bool waitForNextPlayer;
 	private bool gameOver;
 
@@ -22,12 +22,18 @@ public class Turn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Return) && !waitForNextPlayer) {
+			NextTurn ();
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && waitForNextPlayer) {
+			RevealTurnPlayer ();
+		}
 	}
 
 	void NextTurn () {
 		waitForNextPlayer = true;
-		turnPlayer = !turnPlayer;
+		if(turnPlayer == Player.WHITE) turnPlayer = Player.BLACK;
+		else if(turnPlayer == Player.BLACK) turnPlayer = Player.WHITE;
 	}
 
 	void RevealTurnPlayer () {
