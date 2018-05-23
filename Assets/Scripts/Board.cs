20a4fb8044d49d1435abd50c2bfa9de3e7d28bc7
Jake Terpening <jterpeni@uci.edu>
2018-05-22 19:45:51 -0700

    offset stuff

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -18,6 +18,8 @@ public class Board : MonoBehaviour {
 	private Vector2 startDrag;
 	private Vector2 endDrag;
 
+	private Vector3 offset = new Vector3 (45.0f, 35.0f, 0);
+
 	void Start()
 	{
 		selectedPiece = "empty";
@@ -111,7 +113,7 @@ public class Board : MonoBehaviour {
 			return;
 		}
 		RaycastHit hit;
-		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f))
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition + offset), out hit, 25.0f))
 		{
 			mouseOver.x = (int)(hit.point.x);
 			mouseOver.y = (int)(hit.point.z);
@@ -1159,7 +1161,6 @@ public class Board : MonoBehaviour {
 				tileScript.selectSpace (false);
 			}
 		}
-
 	}
 
 	void Update()
